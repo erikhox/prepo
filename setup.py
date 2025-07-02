d3e8c0405829a4cb9a120f90255933f68fb20ba3
erikhox <erikhoxhajj@gmail.com>
2025-07-02 12:19:10 -0400

    forgot to update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="prepo",
-        version="0.1.5",
+        version="0.1.6",
     author="Erik Hoxhaj",
     author_email="erik.hoxhaj@outlook.com",
     description="A package for preprocessing pandas DataFrames",
